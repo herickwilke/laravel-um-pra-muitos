d60c8ff1deff23fe06d05d7c821122570d38f5ef
herick <herickcwilke@gmail.com>
2020-01-02 09:23:31 -0300

    Fim da seçao

--- routes/web.php
+++ routes/web.php
@@ -62,3 +62,44 @@ Route::get('/categoriasprodutos', function () {
             }
     }    
 });
+
+Route::get('/categoriasprodutos/json', function () {
+    $cats = Categoria::with('produtos')->get();
+    return $cats->toJson();
+});
+
+Route::get('/adicionarproduto', function () {
+    $cat = Categoria::find(1);
+    $p = new Produto();
+    $p->nome = "Meu novo produto";
+    $p->estoque = 10;
+    $p->preco = 100;
+    $p->categoria()->associate($cat);
+    $p->save();
+    return $p->toJson();
+});
+
+Route::get('/removerprodutocategoria', function () {
+    $p = Produto::find(9);
+    if(isset($p)){
+        $p->categoria()->dissociate();
+        $p->save();
+        return $p->toJson();
+    }
+    return 'Nothing';
+});
+
+Route::get('/adicionarproduto/{catid}', function ($catid) {
+    $cat = Categoria::with('produtos')->find($catid);
+
+    $p = new Produto();
+    $p->nome = "Meu novo produto adicionado 2";
+    $p->estoque = 40;
+    $p->preco = 780;
+
+    if(isset($cat)){
+        $cat->produtos()->save($p);
+    }
+    $cat->load('produtos');
+    return $cat->toJson();
+});
